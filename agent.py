Fix reward calculation

agent.py
@@ -1,7 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.autograd as autograd
-import numpy as np
 
 from replay_buffer import ReplayBuffer
 from dqn import DQN
@@ -16,7 +14,7 @@ class Agent:
 
         self.model = DQN(env.action_space.n)
 
-        self.optimizer = torch.optim.Adam(self.model.parameters())
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=learning_rate)
         self.loss = nn.MSELoss()
 
     def get_action(self, state):

env.py
@@ -1,11 +1,8 @@
 import gym
 import numpy as np
 
-# inspired by RaveForce: https://github.com/chaosprint/RaveForce/blob/master/Python/raveforce.py
-# also contains many more action spaces
-
-# TO DO: inherit from gym.Space
 class SequenceActions:
+    # TO DO: inherit from gym.Space
     def __init__(self, n_steps):
         self.n = n_steps
     
@@ -13,24 +10,20 @@ class SequenceActions:
         return np.random.rand(self.n,)
 
 class MidiEnv(gym.Env):
-    def __init__(self, n_bars = 4, beats_per_bar = 4):
+    def __init__(self, n_bars = 4, beats_per_bar = 4, metric = "diff"):
         '''Initialises a np array of fixed length for kick and snare'''
         self.n_bars = n_bars
         self.beats_per_bar = beats_per_bar
         
         self.kick_seq = np.array([1, 0, 0, 0] * self.n_bars)
-        # self.kick_seq = np.random.choice([0, 1], size=(get_n_steps(),), p=[3./4, 1./4])
-        self.snare_seq = np.zeros(self.get_n_steps(), dtype = np.int8)
+        self.snare_seq = np.zeros(self.get_n_steps())
         
         self.action_space = SequenceActions(n_steps = self.get_n_steps())
-        self.reward_range = (0.0, 1.0)
-        self.done_threshold = (0.1, 0.9) # episodes that go outside this range should be considered done
+        self.metric = metric
         
-        self.rounding_threshold = 0.5
-
-        # TO DO - how to define the observation space? it's just a np array of length n_steps
-        self.observation_space = None
-
+        if self.metric == "diff":
+            self.reward_range = (0.0, 1.0)
+            self.done_range = (0.2, 0.9)
     
     def get_n_steps(self):
         return self.n_bars * self.beats_per_bar
@@ -42,35 +35,41 @@ class MidiEnv(gym.Env):
         return self.get_n_steps()
 
     def step(self, action : np.ndarray):
-        reward = self.calculate_reward(self.kick_seq, action)
-        _min, _max = self.done_threshold
-        done = not _min <= reward <= _max
         self.snare_seq = action
+        reward = self.calculate_reward(self.kick_seq, action)
+
+        # episodes that go outside this range should be considered done
+        _min, _max = self.done_range
+        done = not _min <= reward <= _max
+
         return self.kick_seq, reward, done, {}
     
     def reset(self):
         self.kick_seq = np.array([1, 0, 0, 0] * self.n_bars)
         # self.kick_seq = np.random.choice([0, 1], size=(self.get_n_steps(),), p=[3./4, 1./4])
-        self.snare_seq = np.zeros(self.get_n_steps(), dtype = np.int8)
+        self.snare_seq = np.zeros(self.get_n_steps())
         return self.get_state()
-        
+
     def close(self):
         return
-    
+
     def calculate_reward(self, kick_seq : np.ndarray, snare_seq: np.ndarray):
+        '''Normalized sum to fit in the reward range'''
         assert kick_seq.shape == snare_seq.shape
-        diff = np.sum(kick_seq - snare_seq)
-        return np.abs(diff / self.get_n_steps()) # normalized sum to fit in the reward range
-    
+
+        if self.metric == "diff":
+            diff = np.abs(kick_seq - snare_seq)
+            return np.sum(diff / self.get_n_steps())
+
     def render(self, log=True):
         '''Renders the np array as a sequence of MIDI notes'''
-        def to_string(seq : np.ndarray):
-            x = lambda s : str(s).strip().replace("1", "x").replace("0", "o")
-            sequence = list(map(x, seq.tolist()))
-            return " ".join(sequence)
-
         if log:
-            print(to_string(np.where(self.snare_seq >= 0.5, 1, 0)), "SNARE")
-            print(to_string(self.kick_seq), "KICK")
+            print(self.to_string(np.where(self.snare_seq >= 0.5, 1, 0)), "SNARE", "\t", np.round(self.snare_seq, 2))
+            print(self.to_string(self.kick_seq), "KICK", "\t", np.round(self.kick_seq, 2))
 
-        return
+        # TO DO: render as MIDI and send on output port
+
+    def to_string(self, seq : np.ndarray):
+        x = lambda s : str(s).strip().replace("1", "x").replace("0", "o")
+        sequence = list(map(x, seq.tolist()))
+        return " ".join(sequence)

train.py
@@ -2,7 +2,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from time import time
 
-def train(env, agent, n_episodes, max_steps, batch_size, render=True, log=True):
+def train(env, agent, n_episodes, max_steps, batch_size, render=True, log=True, save_model=False):
     if log:
         log_interval = n_episodes // 10
 
@@ -17,10 +17,8 @@ def train(env, agent, n_episodes, max_steps, batch_size, render=True, log=True):
         should_explore = np.random.random_sample() < eps
 
         for step in range(max_steps):
-            # exploration
             if should_explore:
                 action = env.action_space.sample()
-            # exploitation
             else:
                 action = agent.get_action(state)
 
@@ -39,11 +37,13 @@ def train(env, agent, n_episodes, max_steps, batch_size, render=True, log=True):
 
         if episode % log_interval == 0:
             if log:
-                print("Episode", episode, 
-                    ":", np.mean(episode_rewards[-1]), 
-                    "" if should_explore else "🤖")
+                action = agent.get_action(state)
+                reward = env.calculate_reward(obs, action)
+                print("Episode", episode, ":", reward)
             if render:
                 env.render()
+            if save_model and episode > 0:
+                print("💾 SAVING MODEL AT EPISODE", episode)
 
 
     if log: